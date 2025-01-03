36f149cd173b6c558d8011e26c83576139fdcfb8
yjunhee <82225260+yjunhee@users.noreply.github.com>
2025-01-03 16:32:52 +0900

    Add files via upload

--- users.py
+++ users.py
@@ -1,12 +1,24 @@
 from flask import Blueprint, request, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
 user = Blueprint('users', __name__, url_prefix='/users')
 
+db = SQLAlchemy()
 
-username = 'Default'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(200))
 
 
-@user.route('/update_username', methods=['POST'])
+@user.route('/userpage', methods=['Get','POST'])
 def update_username():
     global username
-    username = request.form.get('text')
-    return render_template('user_page.html', username= username)
+    username = str(request.form.get('Id', 'guest'))
+    password = request.form.get('userPassword', 'None')
+
+    if username == "admin":
+        return render_template('admin.html')
+    else:
+        return render_template('user_page.html', username=username, password=password)
---
+++ userstate.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy 
+
+dp() = SQLAlchemy()
+
+class User():
+    id = db.Column(db.Integer, primary_key=True)
+    password = 
+    username = username
+    email = email
